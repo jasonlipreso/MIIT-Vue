46270fd4d03fd1915f7cf940cdb65525b73fc014
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2024-09-03 11:21:18 +0800

    OK

--- v1/src/views/HomeView.vue
+++ v1/src/views/HomeView.vue
@@ -14,24 +14,59 @@
     </div>
     <div class="container-bar p-3 m-3">
       <h1 class="text-center">Delete</h1>
+      <button @click="deleteRecord()">Delete</button>
+      <input v-model="id" type="number">
     </div>
   </main>
 </template>
 <script lang="ts">
 
   import axios from "axios";
+  import swal from 'sweetalert';
 
   export default {
     name: "Home",
     data() {
       return {
-        users: {} as any
+        users: {} as any,
+        id: 0
       }
     },
     methods: {
       async getRecord() {
         await axios.get("http://127.0.0.1:8000/api/user/getAll").then( async (response) => {
           this.users = response.data;
+        });
+      },
+      async deleteRecord() {
+        swal({
+          title: "Are you sure?",
+          text: "Delete selected record?",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then( async (willDelete) => {
+          if (willDelete) {
+            await axios.get("http://127.0.0.1:8000/api/user/deleteByID/" + this.id).then( async (response) => {
+              if(response.data?.success) {
+                swal({
+                  title: "Deleted",
+                  text: response.data?.message,
+                  icon: "success",
+                  button: "Aww yiss!",
+                });
+              }
+              else {
+                swal({
+                  title: "Fail",
+                  text: response.data?.message,
+                  icon: "warning",
+                  button: "Aww yiss!",
+                });
+              }
+            });
+          }
         });
       }
     },
