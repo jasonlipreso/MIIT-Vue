81f1e24b9c1a69503b79b9bb187ff71f171b40f7
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2024-09-03 11:08:46 +0800

    Get Record

--- v1/src/views/HomeView.vue
+++ v1/src/views/HomeView.vue
@@ -1,19 +1,45 @@
 <template>
   <main>
-    <h1 class="p-5">GET ALL</h1>
-    <button @click="fetchAll()">Get All</button>
+    <div class="container-bar p-3 m-3 bg-success">
+      <h1 class="text-center bg-warning">GET ALL</h1>
+      <button @click="getRecord()">Get All</button>
+      <table class="table">
+        <tbody>
+          <tr v-for="(user, i) in users" :key="i">
+            <td>{{ user?.fname }}</td>
+            <td>{{ user?.lname }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="container-bar p-3 m-3">
+      <h1 class="text-center">Delete</h1>
+    </div>
   </main>
 </template>
 <script lang="ts">
+
   import axios from "axios";
+
   export default {
     name: "Home",
+    data() {
+      return {
+        users: {} as any
+      }
+    },
     methods: {
-      async fetchAll() {
-        axios.get("http://127.0.0.1:8000/api/user/getAll").then( async (response) => {
-          console.log(response);
+      async getRecord() {
+        await axios.get("http://127.0.0.1:8000/api/user/getAll").then( async (response) => {
+          this.users = response.data;
         });
       }
     },
+    
   }
 </script>
+<style scope>
+  .container-bar {
+    border: 3px solid black;
+  }
+</style>
