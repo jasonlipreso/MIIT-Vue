5259546bc3f1efc4d332fbbde562059985825b18
Jason Lipreso <56417714+jasonlipreso@users.noreply.github.com>
2024-09-03 10:31:21 +0800

    Initial commit

---
+++ v1/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <h1 class="p-5">GET ALL</h1>
+    <button @click="fetchAll()">Get All</button>
+  </main>
+</template>
+<script lang="ts">
+  import axios from "axios";
+  export default {
+    name: "Home",
+    methods: {
+      async fetchAll() {
+        axios.get("http://127.0.0.1:8000/api/user/getAll").then( async (response) => {
+          console.log(response);
+        });
+      }
+    },
+  }
+</script>
